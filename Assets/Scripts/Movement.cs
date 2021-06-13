7fffbe25c5bdea6b3921d72a1953b27f67fe8edd
kjbennicke <56367861+kjbennicke@users.noreply.github.com>
2021-06-13 09:39:46 -0700

    Added walls to game area to test collisions

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField]int moveSpeed = 10;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-      
+      rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float zValue =  Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue, 0, zValue);
+        // float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        // float zValue =  Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        // transform.Translate(xValue, 0, zValue);
+
+        rb.AddRelativeForce(Vector3.forward * moveSpeed);
     }
 }
