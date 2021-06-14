94e05b12a48f85c1d7d72930aedf8b36c9cb387e
kjbennicke <56367861+kjbennicke@users.noreply.github.com>
2021-06-14 07:39:28 -0700

    added functionality for times drop of falling objects

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
-    
+    [SerializeField] int timeSec = 3;
+    MeshRenderer render;
+    Rigidbody grav;
     // Start is called before the first frame update
     void Start()
     {
-        if(Time.time > 3){
-            Debug.Log("3 seconds have elapsed!");
-        }
+        render = GetComponent<MeshRenderer>();
+        grav = GetComponent<Rigidbody>();
+
+        render.enabled = false;
+        grav.useGravity = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+       if(Time.time > timeSec){
+            render.enabled = true;
+            grav.useGravity = true;
+        }
     }
 }
